ed34288e63f5d21b27e6c2a2481a64a16bc30adb
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-07-03 19:09:10 +0300

    Added entity coordinates.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ function App() {
   return (
       <div className="App">
         <a-scene
-            vr-mode-ui="enabled: false"
-            arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+            embedded
+            loading-screen="enabled: false;"
+            arjs="sourceType: webcam; debugUIEnabled: false;"
         >
           <a-text
               value="This content will always face you."
               look-at="[gps-camera]"
               scale="120 120 120"
-              gps-entity-place="latitude: <50,4897373>; longitude: <30,4871598>;"
+              gps-entity-place="latitude: 50.489722; longitude: 30.485833;"
           ></a-text>
           <a-camera gps-camera rotation-reader></a-camera>
         </a-scene>
