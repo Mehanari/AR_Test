53a50533d09a6301d6eec8907da280494ad81a25
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-03 22:40:51 +0300

    Changed coordinates.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
               <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
               <a-entity material='color: red'
                         geometry='primitive: box'
-                        gps-new-entity-place="latitude: 50.473257; longitude: 30.443317"
+                        gps-new-entity-place="latitude: 50.8843233; longitude: 20.6347007"
                         scale="10 10 10"></a-entity>
           </a-scene>
       </div>
