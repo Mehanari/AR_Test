490aadcdbd1adf9b088d3f70626bb4a4e8327915
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-07-03 19:42:56 +0300

    Marker-camera example.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-          <a-scene
-              embedded
-              loading-screen="enabled: false;"
-              arjs="sourceType: webcam; debugUIEnabled: false;"
-          >
-              <a-camera gps-camera='simulateLatitude: 51.049; simulateLongitude: -0.723' rotation-reader></a-camera>
-              <a-text value="This will always face the user." look-at="[gps-camera]" scale="75 75 75"
-                      gps-entity-place="latitude: 51.0491; longitude: -0.723;"></a-text>
+          <a-scene embedded arjs='sourceType: webcam;' renderer='antialias: true; alpha: true; precision: medium;'>
+
+              <a-marker preset="hiro">
+                  <a-sphere position="0 0.5 0" radius="0.5" color="#EF2D5E"></a-sphere>
+                  <a-plane position="0 0 0" rotation="-90 0 0" width="1" height="1" color="#7BC8A4"></a-plane>
+              </a-marker>
+
+              <a-entity camera></a-entity>
           </a-scene>
       </div>
   );
