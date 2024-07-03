72bc16a91ac4d91773381c1c4f0a2b153e5ed4fe
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-07-03 19:55:06 +0300

    Added object with gps coordinates.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
       <div className="App">
           <a-scene embedded arjs='sourceType: webcam;' renderer='antialias: true; alpha: true; precision: medium;'>
 
-              <a-marker preset="hiro">
-                  <a-sphere position="0 0.5 0" radius="0.5" color="#EF2D5E"></a-sphere>
-                  <a-plane position="0 0 0" rotation="-90 0 0" width="1" height="1" color="#7BC8A4"></a-plane>
-              </a-marker>
-
-              <a-entity camera></a-entity>
+              <a-box material="color: red;" scale="5 5 5"
+                     gps-entity-place="latitude: 50.489921; longitude: 30.486889;"></a-box>
+              <a-camera
+                  rotation-reader
+                  gps-camera
+              >
+              </a-camera>
           </a-scene>
       </div>
   );
