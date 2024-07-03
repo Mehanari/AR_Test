a1e747090c9eed8532d0c1e0548de7ce9f46aa86
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-03 22:56:33 +0300

    Changed gps-new-entity-place to gps-projected-entity-place and updated coordinates.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
       <div className="App">
           <a-scene
               vr-mode-ui="enabled: false"
-              arjs='sourceType: webcam;'
-              renderer='antialias: true; alpha: true'>
-              <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+              arjs='sourceType: webcam; videoTexture: true;'
+              renderer='logarithmicDepthBuffer: true; antialias: true; alpha: true'>
+              <a-camera gps-new-camera='gpsMinDistance: 150'></a-camera>
               <a-entity material='color: red'
                         geometry='primitive: box'
-                        gps-new-entity-place="latitude: 50.8843233; longitude: 20.6347007"
+                        gps-projected-entity-place="latitude: 50.473257; longitude: 30.443317"
                         scale="10 10 10"></a-entity>
           </a-scene>
       </div>
