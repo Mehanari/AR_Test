d86deaf822804bce47e88f8c81aa6e8962edd3ba
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-07-03 19:24:20 +0300

    Added a-entity and new location.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-        <a-scene
-            embedded
-            loading-screen="enabled: false;"
-            arjs="sourceType: webcam; debugUIEnabled: false;"
-        >
-          <a-text
-              value="This content will always face you."
-              look-at="[gps-camera]"
-              scale="120 120 120"
-              gps-entity-place="latitude: 50.489722; longitude: 30.485833;"
-          ></a-text>
-          <a-camera gps-camera rotation-reader></a-camera>
-        </a-scene>
+          <a-scene
+              embedded
+              loading-screen="enabled: false;"
+              arjs="sourceType: webcam; debugUIEnabled: false;"
+          >
+              <a-entity
+                  position="0 0 0"
+                  scale="1 1 1"
+                  gltf-model="https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+                  gps-entity-place="latitude: 50.489923; longitude: 30.486881;"
+              ></a-entity>
+              <a-camera gps-camera rotation-reader></a-camera>
+          </a-scene>
       </div>
   );
 }
