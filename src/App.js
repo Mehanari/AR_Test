676b75209653a2adadb792c8f0a5e134e7e38544
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-06 18:55:51 +0300

    Spawn cube nearby.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
+    const [gpsString, setGpsString] = useState("")
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+            console.log("Geolocation not supported");
+        }
+
+        function success(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            setGpsString(`latitude: ${latitude}; longitude: ${longitude}`);
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        }
+
+        function error() {
+            console.log("Unable to retrieve your location");
+        }
+    }, []);
+
     return (
         <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false'
                  renderer='antialias: true; alpha: true'>
             <a-camera gps-new-camera='gpsMinDistance: 150'></a-camera>
             <a-entity material='color: red' geometry='primitive: box'
-                      gps-new-entity-place="latitude: 50.473899; longitude: 30.443537" scale="100 100 100"></a-entity>
+                      gps-new-entity-place={gpsString} scale="5 5 5"></a-entity>
         </a-scene>
     );
 }
