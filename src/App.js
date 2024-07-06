741178de17b4f18e65c55c57429c405772d3cfce
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-06 17:08:48 +0300

    Added logs text.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import {useState} from "react";
 function App() {
 
   const [testEntityAdded, setTestEntityAdded] = useState(false);
+  const [logText, setLogText] = useState('No logs yet.');
 
   //gps-camera-update-position handler
   const onCameraUpdate = e => {
       alert('onCameraUpdate')
       if(!testEntityAdded) {
-          console.log(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+          setLogText(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
           // Add a box to the north of the initial GPS position
           const entity = document.createElement("a-box");
           entity.setAttribute("scale", "1 1 1");
-          entity.setAttribute('material', 'color: \'red\'');
+          entity.setAttribute('material', 'color: red');
           entity.setAttribute('gps-new-entity-place',
                 `latitude: ${e.detail.position.latitude + 0.001}; longitude: ${e.detail.position.longitude}`);
           document.querySelector("a-scene").appendChild(entity);
@@ -27,7 +28,8 @@ function App() {
               vr-mode-ui="enabled: false"
               arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false; '
               renderer='logarithmicDepthBuffer: true; antialias: true; alpha: true'>
-              <a-camera gps-new-camera='gpsMinDistance: 50' gps-camera-update-position={onCameraUpdate} ></a-camera>
+              <a-text value={logText} position="-0.5 0.5 -1" align="center"></a-text>
+              <a-camera gps-new-camera='gpsMinDistance: 50' gps-camera-update-position={onCameraUpdate}></a-camera>
 
           </a-scene>
       </div>
