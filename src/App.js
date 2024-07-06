ed19d8598f44d7a51df0a41b3e8b1df0f23a1e0c
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-06 18:22:43 +0300

    Using gps-entity-place tag.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
                  renderer='antialias: true; alpha: true'>
             <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
             <a-entity material='color: red' geometry='primitive: box'
-                      gps-new-entity-place="latitude: 50.473370; longitude: 30.443225" scale="10 10 10"></a-entity>
+                      gps-entity-place="latitude: 50.473370; longitude: 30.443225" scale="10 10 10"></a-entity>
         </a-scene>
     );
 }
