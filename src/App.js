5aadaf00fe2f17112e605d72f3a6480cb73e5b1e
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-06 15:11:33 +0300

    Added videoTexture=true; and downgraded a-frame. Also repositioned the sphere.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
       <div className="App">
           <a-scene
               vr-mode-ui="enabled: false"
-              arjs='sourceType: webcam;'
+              arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false; '
               renderer='logarithmicDepthBuffer: true; antialias: true; alpha: true'>
-              <a-camera gps-new-camera='gpsMinDistance: 150'></a-camera>
+              <a-camera gps-new-camera='gpsMinDistance: 50'></a-camera>
               <a-entity material='color: red'
                         geometry='primitive: box'
                         gps-new-entity-place="latitude: 50.473186; longitude: 30.443543"
                         scale="10 10 10"></a-entity>
               <a-sphere color="yellow"
                         radius="5"
-                        gps-new-entity-place="latitude: 50.473353; longitude: 30.443280"
+                        gps-new-entity-place="latitude: 50.473942; longitude: 30.443548"
               ></a-sphere>
           </a-scene>
       </div>
