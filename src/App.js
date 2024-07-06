1e7b953bb2ef31da984d0d3a58de44a5e1652a05
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-06 18:29:23 +0300

    Set another coordinates.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
     return (
         <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false'
                  renderer='antialias: true; alpha: true'>
-            <a-camera gps-new-camera='gpsMinDistance: 50'></a-camera>
+            <a-camera gps-new-camera='gpsMinDistance: 150'></a-camera>
             <a-entity material='color: red' geometry='primitive: box'
-                      gps-new-entity-place="latitude: 50.473370; longitude: 30.443225" scale="10 10 10"></a-entity>
+                      gps-new-entity-place="latitude: 50.478997; longitude: 30.434987" scale="10 10 10"></a-entity>
         </a-scene>
     );
 }
