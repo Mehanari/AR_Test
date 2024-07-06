c02e07584c96bc7f9b2901ee85a6438d5e6a3cbf
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-07-06 15:46:30 +0300

    Made cube spawn nearby your position.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [testEntityAdded, setTestEntityAdded] = useState(false);
+
+  //gps-camera-update-position handler
+  const onCameraUpdate = e => {
+      if(!testEntityAdded) {
+          alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+          // Add a box to the north of the initial GPS position
+          const entity = document.createElement("a-box");
+          entity.setAttribute("scale", "1 1 1");
+          entity.setAttribute('material', 'color: \'red\'');
+          entity.setAttribute('gps-new-entity-place',
+                `latitude: ${e.detail.position.latitude + 0.001}; longitude: ${e.detail.position.longitude}`);
+          document.querySelector("a-scene").appendChild(entity);
+          setTestEntityAdded(true);
+      }
+  }
+
   return (
       <div className="App">
           <a-scene
               vr-mode-ui="enabled: false"
               arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false; '
               renderer='logarithmicDepthBuffer: true; antialias: true; alpha: true'>
-              <a-camera gps-new-camera='gpsMinDistance: 50'></a-camera>
-              <a-entity material='color: red'
-                        geometry='primitive: box'
-                        gps-new-entity-place="latitude: 50.489325; longitude: 30.487905"
-                        scale="10 10 10"></a-entity>
-              <a-sphere color="yellow"
-                        radius="5"
-                        gps-new-entity-place="latitude: 50.473942; longitude: 30.443548"
-              ></a-sphere>
+              <a-camera gps-new-camera='gpsMinDistance: 50' gps-camera-update-position={onCameraUpdate} ></a-camera>
+
           </a-scene>
       </div>
   );
